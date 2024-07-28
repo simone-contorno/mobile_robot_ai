5aca1a2c44aab43260284c30ad8677d5f9058239
Simone <simone.contorno@outlook.it>
2024-07-28 17:35:50 +0200

    Implemented the ai control. To be improved.

--- scripts/open_ai_control.py
+++ scripts/open_ai_control.py
@@ -8,39 +8,37 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import PoseStamped 
+from geometry_msgs.msg import PoseStamped, Pose, Twist
 
 from tf_transformations import euler_from_quaternion
 
-from nav2_msgs.action import NavigateToPose
-
-"""
-start = time.time()
-
 client = OpenAI()
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  #model="gpt-4o-mini",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+ai_system = ("You are model to compute a safe control commands for a mobile robot in a static environment, endowed with LiDAR.\n" + 
+             "The control commands must be: linear velocities along x and y axis (m/s), and angular velocity (rad/s) around z axis.\n" +
+             "You know your actual pose, your goal pose and the data given form the laser scanner.")
+print(ai_system)
 
-print(completion.choices[0].message)
-
-end = time.time()
-diff = end - start
-
-# Print time in milliseconds
-print("Time = " + str(diff * 1000) + " ms")
-"""
+ai_user = ("Compute the safe trajectory to reach the goal knowing that the robot pose, the goal pose and the laser data.\n" +
+           "Return the control commands sequence as 3 variables: linear_x linear_y angular_z, considering a specific time step in seconds.\n" +
+           "Return the result without any explanation as: linear_x linear_y angular_z")
+print(ai_user)
 
 class OpenAIControl(Node):
 
     def __init__(self):
         super().__init__('open_ai_control')
+        
+        # Private variables
+        self.goal_x = None
+        self.goal_y = None
+        self.goal_theta = None
+        
+        self.ranges = None
+        self.pose = None
+        self.goal = None
+        
+        self.time_step = 1
         
 		# Get ranges values
         self.subscription_ranges= self.create_subscription(
@@ -56,15 +54,27 @@ class OpenAIControl(Node):
             self.odom_callback,
             10)
         
-        # Get goal values
-        self.subscription_goal = self.create_subscription(
+        # Get goal pose values
+        self.subscription_goal_pose = self.create_subscription(
             PoseStamped,
             'goal_pose',
+            self.goal_pose_callback,
+            10)
+        
+        # Get goal values
+        self.subscription_goal = self.create_subscription(
+            Pose,
+            'goal',
             self.goal_callback,
             10)
-         
+        
+        # Create the publisher
+        self.publisher_cmd = self.create_publisher(Twist, "/cmd_vel", 10)
+        
     def ranges_callback(self, msg):
-        self.get_logger().info("[Ranges] I heard new  " + str(msg.data.size()) + " values")
+        self.ranges = msg.data
+        #self.get_logger().info("[Ranges] I heard new values")
+        # TODO: check if still receiving data
         
     def odom_callback(self, msg):
         x = msg.pose.pose.position.x
@@ -76,11 +86,13 @@ class OpenAIControl(Node):
         qz = msg.pose.pose.orientation.z
         qw = msg.pose.pose.orientation.w
         
-        (r, p, y) = euler_from_quaternion([qx, qy, qz, qw])
-    
-        self.get_logger().info("[Odom] I heard new pose: (" + str(x) + ", " + str(y) + ", " + str(z) + ", " + str(y) + ")")
+        (roll, pitch, theta) = euler_from_quaternion([qx, qy, qz, qw])
 
-    def goal_callback(self, msg):
+        self.pose = (x, y, theta)
+        # TODO: Check if still receiving data
+        #self.get_logger().info("[Odom] I heard new pose: (" + str(x) + ", " + str(y) + ", " + str(theta) + ")")
+
+    def goal_pose_callback(self, msg):
         x = msg.pose.position.x
         y = msg.pose.position.y
         z = msg.pose.position.z
@@ -90,10 +102,96 @@ class OpenAIControl(Node):
         qz = msg.pose.pose.orientation.z
         qw = msg.pose.pose.orientation.w
         
-        (r, p, y) = euler_from_quaternion([qx, qy, qz, qw])
+        (roll, pitch, theta) = euler_from_quaternion([qx, qy, qz, qw])
         
-        self.get_logger().info("[Goal] I heard new goal: (" + str(x) + ", " + str(y) + ", " + str(z) + ", " + str(y) + ")")
+        self.get_logger().info("[Goal Pose] I heard new goal: (" + str(x) + ", " + str(y) + ", " + str(theta) + ")")
+    
+    def goal_callback(self, msg):
+        x = msg.position.x
+        y = msg.position.y
+        z = msg.position.z
+        
+        qx = msg.orientation.x
+        qy = msg.orientation.y
+        qz = msg.orientation.z
+        qw = msg.orientation.w
+        
+        (roll, pitch, theta) = euler_from_quaternion([qx, qy, qz, qw])
+        
+        if self.goal_x != x or self.goal_y != y or self.goal_theta != theta:
+            self.goal_x = x
+            self.goal_y = y
+            self.goal_theta = theta
+            self.goal = (x, y, theta)
+            self.get_logger().info("[Goal] I heard new goal: (" + str(x) + ", " + str(y) + ", " + str(theta) + ")")
 
+        if self.pose != None and self.ranges != None:
+            print("[Odom] Current pose: (" + str(self.pose[0]) + ", " + str(self.pose[1]) + ", " + str(self.pose[2]) + ")")
+            global ai_user
+            #print("System = \n" + str(ai_system) + "\n")
+            
+            # Add goal and pose information to the user
+            self.ai_user = ai_user + ("Here your data: " +
+                                "Time step: " + str(self.time_step) + " s\n" +
+                                "Goal: (" + str(self.goal[0]) + ", " + str(self.goal[1]) + ", " + str(self.goal[2]) + ")\n" +
+                                "Pose: (" + str(self.pose[0]) + ", " + str(self.pose[1]) + ", " + str(self.pose[2]) + ")\n" +
+                                "Ranges: ("
+                                )
+            
+            # Add ranges information to the user
+            for i in range(len(self.ranges)):
+                self.ai_user = self.ai_user + str(self.ranges[i]) + ", "
+            self.ai_user = self.ai_user + ")\n"
+            
+            #print("Request = \n" + str(ai_user) + "\n")
+            
+            start = time.time()
+
+            completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            #model="gpt-4o-mini",
+            messages=[
+                    {"role": "system", "content": ai_system},
+                    {"role": "user", "content": self.ai_user}
+                ]
+            )
+
+            cmd_vel = completion.choices[0].message.content
+            print("Controls (x, y, theta) = " + cmd_vel)
+            # Clear the message
+            #cmd_vel = completion.choices[0].message.content.replace("[", "").replace("]", "").replace(" ", "").split(",")
+            if ", " in cmd_vel:
+                cmd_vel = cmd_vel.replace(",", "")
+            if "(" in cmd_vel:
+                cmd_vel = cmd_vel.replace("(", "")
+            if ")" in cmd_vel:
+                cmd_vel = cmd_vel.replace(")", "")
+            if "[" in cmd_vel:
+                cmd_vel = cmd_vel.replace("[", "")
+            if "]" in cmd_vel:
+                cmd_vel = cmd_vel.replace("]", "")
+                
+            cmd_vel = completion.choices[0].message.content.split(" ")
+            print("Linear velocity x = " + cmd_vel[0] + " m/s\n" +
+                  "Linear velocity y = " + cmd_vel[1] + " m/s\n" +
+                  "Angular velocity z = " + cmd_vel[2] + " rad/s\n")
+                  
+
+            end = time.time()
+            diff = end - start
+
+            # Print time in milliseconds
+            print("Time = " + str(diff) + " s")
+            self.time_step = diff
+            
+            # Publish control commands
+            cmd = Twist()
+            cmd.linear.x = float(cmd_vel[0])
+            cmd.linear.y = float(cmd_vel[1])
+            cmd.angular.z = float(cmd_vel[2])
+            self.publisher_cmd.publish(cmd)
+
+        
 def main(args=None):
     # Initialize the node
     rclpy.init(args=args)
--- scripts/set_goal.py
+++ scripts/set_goal.py
@@ -6,6 +6,8 @@ import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
 from nav2_msgs.action import NavigateToPose
+from geometry_msgs.msg import Pose
+from tf_transformations import quaternion_from_euler
 
 class SetGoal(Node):
 
@@ -15,19 +17,43 @@ class SetGoal(Node):
         # Declare parameters
         self.declare_parameter('x', -1.0)
         self.declare_parameter('y', 0.0)
-        self.declare_parameter('theta', 1.0)
+        self.declare_parameter('theta', 0.0)
         
         # Get parameters
         self.x = self.get_parameter('x').get_parameter_value().double_value
         self.y = self.get_parameter('y').get_parameter_value().double_value
         self.theta = self.get_parameter('theta').get_parameter_value().double_value
         
+        # Create the goal
+        self.msg = Pose()
+        self.msg.position.x = self.x
+        self.msg.position.y = self.y
+        self.msg.position.z = 0.0
+        (qx, qy, qz, qw) = quaternion_from_euler(0.0, 0.0, self.theta)
+        self.msg.orientation.x = qx
+        self.msg.orientation.y = qy
+        self.msg.orientation.z = qz
+        self.msg.orientation.w = qw
+        
+        # Create the publisher
+        self.publisher_ = self.create_publisher(Pose, "/goal", 10)
+        timer_period = 1.0  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        
+    def timer_callback(self):
+        self.publisher_.publish(self.msg)
+        self.get_logger().info("Publishing: (" + str(self.x) + ", " + str(self.y) + ", " + str(self.theta) + ")")
+            
+        """
+        # Send the action
         self.feedback_callback = None # Define the function to ged on-going feedback
         self._action_client = ActionClient(self, NavigateToPose, '/navigate_to_pose')
         self.get_logger().info("Waiting for action server...")
         self._action_client.wait_for_server()
         self.send_goal()
+        """
 
+    """
     def send_goal(self):
         goal_msg = NavigateToPose.Goal()
         goal_msg.pose.pose.position.x = float(self.x)
@@ -40,7 +66,7 @@ class SetGoal(Node):
         self.get_logger().info("Sending goal (x,y,theta): (" + str(self.x) + ", " + str(self.y) + ", " + str(self.theta) + ")")
         future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
         rclpy.spin_until_future_complete(self, future)
-
+    """
 def main(args=None):
     # Initialize the node
     rclpy.init(args=args)
