0086aa0f2577567366ec66524229296e3d4eced6
Simone <simone.contorno@outlook.it>
2024-10-26 15:39:03 +0200

    Added new packages to be installed for ML control.

--- config.sh
+++ config.sh
@@ -1,3 +1,7 @@
 sudo apt install ros-$1-navigation2
 sudo apt install ros-$1-nav2-bringup
 sudo apt install ros-$1-turtlebot3-gazebo
+pip install pandas
+pip install numpy
+pip install scikit-learn
+#pip install matplotlib
